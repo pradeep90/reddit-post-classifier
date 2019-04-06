4e768c1e54f9f8156cc950f3c57283fb25e4eb9c
Mohammad Haseeb <mhaseeb@data.cs.purdue.edu>
2019-04-06 12:30:55 -0400

    preprocessing: implement stemming and lemmatization; both take very long

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -5,13 +5,19 @@ import re
 import pandas as pd
 import numpy as np
 
-DEBUG		= True
+import time
 
-PATH_TO_DATA		= '../../data/'
-DATA_FILE_NAME		= 'rspct.tsv' 
-DATA				= PATH_TO_DATA+DATA_FILE_NAME 
+import nltk
+nltk.download('wordnet') #TODO: should add this to the make file?
 
-OUTPUT_FILE_NAME 	= 'rspct_preprocessed.tsv'
+DEBUG	= True
+TIME_DEBUG = True
+
+PATH_TO_DATA 	= '../../data/'
+DATA_FILE_NAME 	= 'rspct.tsv' 
+DATA 		= PATH_TO_DATA+DATA_FILE_NAME 
+
+OUTPUT_FILE_NAME	= 'rspct_preprocessed.tsv'
 
 
 def lowercase(df):
@@ -31,7 +37,7 @@ def remove_nums(df):
 
 
 def remove_tags_puncts_whites(text):
-	# to remove &gt tags. TODO: might be other such tags that need to be removed
+	# to remove &gt tags. TODO: there might be other such tags that need to be removed
 	p1 = re.compile(r'&gt')
 	text = p1.sub('', text)
 
@@ -58,14 +64,74 @@ def remove_tags_puncts_whitespaces(data):
 	return data
 
 
+def stem_text(text):
+	stemmer = nltk.stem.PorterStemmer()
+	tokenized_text = nltk.tokenize.word_tokenize(text)
+	stemmed_words = [stemmer.stem(word) for word in tokenized_text]
+	return ' '.join(stemmed_words)
+
+
+def stem_data(data):
+	"""
+	Replace all words with their stem words
+	"""
+	if DEBUG:
+		print ('Stemming the data')
+	for col in data.columns:
+		if col not in ['id', 'subreddit']:
+			data[col] = data[col].apply(stem_text)
+	return data	
+
+
+def lemmatize_text(text):
+	lemmatizer = nltk.stem.WordNetLemmatizer()
+	tokenized_text=nltk.tokenize.word_tokenize(text)
+	lemmatized_words = [lemmatizer.lemmatize(word) for word in tokenized_text]
+	return ' '.join(lemmatized_words)
+
+
+def lemmatize_data(data):
+	if DEBUG:
+		print ('Lemmatizing the data')
+	for col in data.columns:
+		if col not in ['id', 'subreddit']:
+			data[col] = data[col].apply(lemmatize_text)
+	return data		
+
+
 def preprocess(data):
-	data = lowercase(data) 
-	#data = remove_nums(data)
+
+	t0 = time.time()
+	data = lowercase(data) 	
+	t1 = time.time()
+	if TIME_DEBUG:
+		print('That took time: {}'.format(t1-t0))
+
+	# t0 = time.time()
+	# data = remove_nums(data)
+	# t1 = time.time()
+	# if TIME_DEBUG:
+	# 	print('That took time: {}'.format(t1-t0))
+
+	t0 = time.time()
 	data = remove_tags_puncts_whitespaces(data)
+	t1 = time.time()
+	if TIME_DEBUG:
+		print('That took time: {}'.format(t1-t0))
 
 	# DISCUSS: tokenization and stop-word-removal to be done by the library itself?
 
-	# TODO: stemming, lemmatization
+	# t0 = time.time()
+	# data = stem_data(data)
+	# t1 = time.time()
+	# if DEBUG:
+	# 	print('That took time: {}'.format(t1-t0))
+
+	t0 = time.time()
+	data = lemmatize_data(data)
+	t1 = time.time()
+	if DEBUG:
+		print('That took time: {}'.format(t1-t0))
 
 	return data
 
@@ -74,12 +140,17 @@ def main():
 	if DEBUG:
 		print('Reading the data')
 
+	t0 = time.time()
+
 	df = pd.read_csv(DATA, sep='\t')
 	
 	preprocessed_df = preprocess(df)
 
 	preprocessed_df.to_csv(OUTPUT_FILE_NAME, sep='\t', index=False)
 
+	t1 = time.time()
+	if TIME_DEBUG:
+		print('Total time taken: {}'.format(t1-t0))
 
 if __name__ == '__main__':
 	main()
--- src/preprocessing/tests/test_preprocessing.py
+++ src/preprocessing/tests/test_preprocessing.py
@@ -28,6 +28,30 @@ class TestPreprocessing(unittest.TestCase):
 		self.assertEqual(both_equal, True)
 
 
+	def test_stemming(self):
+		expected_res = 'hi there the usual long time lerker first time poster be kind etc sorri if thi isnt the right placealright here the stori im an independ develop who produc my own softwar were go to call me well mei work with dev who help to produc softwar with me we use popularversioncontrolwer tri to remov a branch that wa creat by mistak the branch is beta1 we want just beta me dev can you renam that branch becaus were go to use just two i dont want to keep up with 80 quintilian branch dev sure one secondf minut later dev i want beta1 to die me what happenedlot of remov dialog where dev explain what he did me did you tri popularversioncontrol with u dev that would be whyin short alway check your command line switchesthey are import' 
+
+		data = [[expected_res]]
+		expected_df = pd.DataFrame(data, columns= ['selfpost'])
+
+		res = preprocessing.preprocess(self.df)
+		both_equal = res.equals(expected_df)
+
+		self.assertEqual(both_equal, True)
+
+
+	def test_lemmatization(self):
+		expected_res = 'hi there the usual long time lerker first time poster be kind etc sorry if this isnt the right placealright here the story im an independent developer who produce my own software were going to call me well mei work with dev who help to produce software with me we use popularversioncontrolwere trying to remove a branch that wa created by mistake the branch is beta1 we want just beta me dev can you rename that branch because were going to use just two i dont want to keep up with 80 quintilian branch dev sure one secondfive minute later dev i want beta1 to die me what happenedlots of removed dialog where dev explains what he did me did you try popularversioncontrol with u dev that would be whyin short always check your command line switchesthey are important'
+
+		data = [[expected_res]]
+		expected_df = pd.DataFrame(data, columns= ['selfpost'])
+
+		res = preprocessing.preprocess(self.df)
+		both_equal = res.equals(expected_df)
+
+		self.assertEqual(both_equal, True)
+		
+
 	def test_preprocessing_whole(self):
 		"""
 		Tests the whole preprocessing pipelines and compares if the final result is as expected.
