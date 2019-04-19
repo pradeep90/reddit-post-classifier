14496dc96c8986f70d72dff622f117a80584d2df
Mohammad Haseeb <mhaseeb@data.cs.purdue.edu>
2019-04-19 16:22:04 -0400

    tests

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,5 +1,7 @@
 # Source: https://medium.com/@datamonsters/text-preprocessing-in-python-steps-tools-and-examples-bf025f872908
 
+# TODO: stemming and lemmatization for apostraphied words e.g. isn't, don't not giving correct results
+
 import re 
 
 import pandas as pd
@@ -17,7 +19,7 @@ PATH_TO_DATA 	= '../../data/'
 DATA_FILE_NAME 	= 'rspct.tsv' 
 DATA 		= PATH_TO_DATA+DATA_FILE_NAME 
 
-OUTPUT_FILE_NAME	= 'rspct_preprocessed.tsv'
+OUTPUT_FILE_NAME	= 'rspct_preprocessed_lemmatized.tsv'
 
 
 def lowercase(df):
@@ -37,18 +39,27 @@ def remove_nums(df):
 
 
 def remove_tags_puncts_whites(text):
+	text = text.strip()
+	
 	# to remove &gt tags. TODO: there might be other such tags that need to be removed
-	p1 = re.compile(r'&gt')
-	text = p1.sub('', text)
+	p1 = re.compile(r'&gt|&amp|&lt')
+	text = p1.sub(' ', text)
 
 	# to remove tags inside {}, [] and HTML tags
 	p2 = re.compile(r'[<{\[].*?[>}\]]') 
-	text = p2.sub('', text)
+	text = p2.sub(' ', text)
+
+	# remove single quotes only if they preceded or follow a word
+	text1 = re.sub(r"((?P<a>\s)'(?P<x>\w))|((?P<y>\w)'(?P<b>\s))|((?P<c>\s)'(?P<d>\s))", r'\g<a>\g<x>\g<y>\g<b>\g<c>\g<d>', text)
+	text = re.sub(r"((?P<a>\s)'(?P<x>\w))|((?P<y>\w)'(?P<b>\s))|((?P<c>\s)'(?P<d>\s))", r'\g<a>\g<x>\g<y>\g<b>\g<c>\g<d>', text1)
+	text = text.strip("'")
 
 	# to remove punctuations (after removing tags etc.)
-	puncts_to_remove = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
-	text = text.translate({ord(c): None for c in puncts_to_remove})
+	#puncts_to_remove = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+	puncts_to_remove = """!"#$%&()*+,-./:;<=>?@[\]^_`{|}~"""
+	text = text.translate({ord(c): ' ' for c in puncts_to_remove})
 
+	#print('ret:', text.strip())
 	return text.strip()
 
 
@@ -68,6 +79,7 @@ def stem_text(text):
 	stemmer = nltk.stem.PorterStemmer()
 	tokenized_text = nltk.tokenize.word_tokenize(text)
 	stemmed_words = [stemmer.stem(word) for word in tokenized_text]
+	#print('stemmed=',' '.join(stemmed_words))
 	return ' '.join(stemmed_words)
 
 
@@ -87,6 +99,7 @@ def lemmatize_text(text):
 	lemmatizer = nltk.stem.WordNetLemmatizer()
 	tokenized_text=nltk.tokenize.word_tokenize(text)
 	lemmatized_words = [lemmatizer.lemmatize(word) for word in tokenized_text]
+	#print('lemmatized:',' '.join(lemmatized_words))
 	return ' '.join(lemmatized_words)
 
 
@@ -105,7 +118,7 @@ def preprocess(data):
 	data = lowercase(data) 	
 	t1 = time.time()
 	if TIME_DEBUG:
-		print('That took time: {}'.format(t1-t0))
+		print('Lowercasing took time: {}'.format(t1-t0))
 
 	# t0 = time.time()
 	# data = remove_nums(data)
@@ -117,21 +130,21 @@ def preprocess(data):
 	data = remove_tags_puncts_whitespaces(data)
 	t1 = time.time()
 	if TIME_DEBUG:
-		print('That took time: {}'.format(t1-t0))
-
-	# DISCUSS: tokenization and stop-word-removal to be done by the library itself?
-
-	# t0 = time.time()
-	# data = stem_data(data)
-	# t1 = time.time()
-	# if DEBUG:
-	# 	print('That took time: {}'.format(t1-t0))
+		print('Removing punctuations took time: {}'.format(t1-t0))
+		
+	"""
+	t0 = time.time()
+	data = stem_data(data)
+	t1 = time.time()
+	if DEBUG:
+		print('Stemming took time: {}'.format(t1-t0))
+	"""
 
 	t0 = time.time()
 	data = lemmatize_data(data)
 	t1 = time.time()
 	if DEBUG:
-		print('That took time: {}'.format(t1-t0))
+		print('Lemmatization took time: {}'.format(t1-t0))
 
 	return data
 
@@ -143,6 +156,7 @@ def main():
 	t0 = time.time()
 
 	df = pd.read_csv(DATA, sep='\t')
+	#df = pd.read_csv(DATA, sep='\t', nrows=1000)
 	
 	preprocessed_df = preprocess(df)
 
--- src/preprocessing/tests/test_preprocessing.py
+++ src/preprocessing/tests/test_preprocessing.py
@@ -13,11 +13,11 @@ class TestPreprocessing(unittest.TestCase):
 	data = [[post_to_test]]
 	df = pd.DataFrame(data, columns= ['selfpost'])
 	
-
+	"""
 	def test_lower_remove_tags_puncts_whites(self):
-		"""
+		'''
 		Tests if lower-casing and removing punctuations, and tags
-		"""
+		'''
 		expected_res = 'hi there  the usual long time lerker first time poster be kind etc sorry if this isnt the right placealright heres the story im an independent developer who produces my own software were going to call me well mei work with dev who helps to produce software with me we use popularversioncontrolwere trying to remove a branch that was created by mistake  the branch is beta1 we want just beta me dev can you rename that branch because were going to use just two i dont want to keep up with 80 quintilian branches   dev sure one secondfive minutes later dev  i want beta1 to die   me what happenedlots of removed dialog where dev explains what he did me did you try popularversioncontrol with u   dev  that would be whyin short always check your command line switchesthey are important'
 		data = [[expected_res]]
 		expected_df = pd.DataFrame(data, columns= ['selfpost'])
@@ -26,10 +26,11 @@ class TestPreprocessing(unittest.TestCase):
 		both_equal = res.equals(expected_df)
 
 		self.assertEqual(both_equal, True)
+	"""
 
 
 	def test_stemming(self):
-		expected_res = 'hi there the usual long time lerker first time poster be kind etc sorri if thi isnt the right placealright here the stori im an independ develop who produc my own softwar were go to call me well mei work with dev who help to produc softwar with me we use popularversioncontrolwer tri to remov a branch that wa creat by mistak the branch is beta1 we want just beta me dev can you renam that branch becaus were go to use just two i dont want to keep up with 80 quintilian branch dev sure one secondf minut later dev i want beta1 to die me what happenedlot of remov dialog where dev explain what he did me did you tri popularversioncontrol with u dev that would be whyin short alway check your command line switchesthey are import' 
+		expected_res = "hi there the usual long time lerker first time poster be kind etc sorri if thi is n't the right place alright here 's the stori i 'm an independ develop who produc my own softwar we 're go to call me well me i work with dev who help to produc softwar with me we use popularversioncontrol we 're tri to remov a branch that wa creat by mistak the branch is beta1 we want just beta me dev can you renam that branch becaus we 're go to use just two i do n't want to keep up with 80 quintilian branch dev sure one second five minut later dev i want beta1 to die me what happen lot of remov dialog where dev explain what he did me did you tri popularversioncontrol with u dev that would be whi in short alway check your command line switch they are import"
 
 		data = [[expected_res]]
 		expected_df = pd.DataFrame(data, columns= ['selfpost'])
@@ -41,7 +42,7 @@ class TestPreprocessing(unittest.TestCase):
 
 
 	def test_lemmatization(self):
-		expected_res = 'hi there the usual long time lerker first time poster be kind etc sorry if this isnt the right placealright here the story im an independent developer who produce my own software were going to call me well mei work with dev who help to produce software with me we use popularversioncontrolwere trying to remove a branch that wa created by mistake the branch is beta1 we want just beta me dev can you rename that branch because were going to use just two i dont want to keep up with 80 quintilian branch dev sure one secondfive minute later dev i want beta1 to die me what happenedlots of removed dialog where dev explains what he did me did you try popularversioncontrol with u dev that would be whyin short always check your command line switchesthey are important'
+		expected_res = "hi there the usual long time lerker first time poster be kind etc sorry if this is n't the right place alright here 's the story i 'm an independent developer who produce my own software we 're going to call me well me i work with dev who help to produce software with me we use popularversioncontrol we 're trying to remove a branch that wa created by mistake the branch is beta1 we want just beta me dev can you rename that branch because we 're going to use just two i do n't want to keep up with 80 quintilian branch dev sure one second five minute later dev i want beta1 to die me what happened lot of removed dialog where dev explains what he did me did you try popularversioncontrol with u dev that would be why in short always check your command line switch they are important"
 
 		data = [[expected_res]]
 		expected_df = pd.DataFrame(data, columns= ['selfpost'])
@@ -50,20 +51,7 @@ class TestPreprocessing(unittest.TestCase):
 		both_equal = res.equals(expected_df)
 
 		self.assertEqual(both_equal, True)
-		
 
-	def test_preprocessing_whole(self):
-		"""
-		Tests the whole preprocessing pipelines and compares if the final result is as expected.
-		"""
-		expected_res = ''
-		data = [[expected_res]]
-		expected_df = pd.DataFrame(data, columns= ['selfpost'])
-
-		res = preprocessing.preprocess(self.df)
-		both_equal = res.equals(expected_df)
-
-		self.assertEqual(both_equal, True)
 
 if __name__ == '__main__':
 	unittest.main()
